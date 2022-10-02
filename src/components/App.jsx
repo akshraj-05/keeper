de18dbf7091a7122f9bbff5186a25fb78f7f8ed6
akshraj chavda <90257725+akshraj-05@users.noreply.github.com>
2022-10-02 08:37:38 +0530

    notes data add and App.jsx and Note.jsx update to handle props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function everyNote(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Footer />
-            <Note />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
 
+      {notes.map(everyNote)}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
